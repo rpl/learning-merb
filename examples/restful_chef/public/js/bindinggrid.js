469d45553e9ea6cae08fe94196ac7c494a39bbb8
Andrea Fazzi <andrea@moon.(none)>
2008-09-14 20:05:02 +0200

    introduced private methods in GridForm 'class'

--- examples/restful_chef/public/js/bindinggrid.js
+++ examples/restful_chef/public/js/bindinggrid.js
@@ -2,8 +2,7 @@ Ext.ux.GridFormBinding = Ext.extend(Ext.Panel, {
 
   initComponent: function() {
 
-    // Apply configuration.
-
+    // Apply prototype(?) configuration.
     Ext.apply(this,{
 		    frame: true,
 		    labelAlign: 'left',
@@ -13,94 +12,116 @@ Ext.ux.GridFormBinding = Ext.extend(Ext.Panel, {
     });
 
     // Call superclass constructor.
-
     Ext.ux.GridFormBinding.superclass.initComponent.call(this, arguments);
 
-    // Add the form component in the panel and configure it.
+    initForm.call(this);
+    initGrid.call(this);
 
-    var form = this.add({
-	       xtype: 'form',
-	       id: 'form',
-	       labelWidth: 75,
-	       frame: true,
-	       title: 'Nuova ricetta',
-	       bodyStyle:'padding:5px',
-	       width: 500,
-	       defaults: {width: 230},
-	       defaultType: 'textfield',
-	       items: [{fieldLabel: 'Ricetta', name: 'name', allowBlank: false}, {hideLabel: true, hidden: true, name: 'id'}]
-    });
+    // private methods
 
-    // Code block used to reloading datastore. The block is called in the scope of this.
+    // Add the grid component into the panel and configure it.
+    function initGrid() {
 
-    var dsReload = function() {
-      this.reload();
-    };
+      // Add the grid component and configure it. Assign rsModel to sm configuration key.
+
+      this.add({
+		 xtype: 'grid',
+		 id: 'grid',
+		 ds: this.ds,
+		 title: 'Tutte le ricette',
+		 width: 500,
+		 height: 200,
+		 columns: [{header: 'Nome', width: 120, sortable: true, dataIndex: 'name'}],
+		 sm: rsModel.call(this)
+	       });
+
+    }
+
+    // Instantiate a RowSelectionModel object and attach to it a rowselect event.
+    function rsModel() {
+
+      // The currently selected id.
+      var currId;
+
+      // Create a RowSelectionModel object.
+      var rsModel = new Ext.grid.RowSelectionModel({singleSelect: true});
+
+      // Load record data in the form's fields when a 'rowselect' event occurs.
+      rsModel.addListener('rowselect',
+			  function(sm, row, rec) {
+			    Ext.getCmp("form").getForm().loadRecord(rec);
+	    		    this.currId = rec.data.id;
+			  },
+			  this);
+      return rsModel;
+    }
+
+    // Add the form component into the panel and configure it.
+    function initForm() {
+
+      this.form = this.add({
+			    xtype: 'form',
+			    id: 'form',
+			    labelWidth: 75,
+			    frame: true,
+			    title: 'Nuova ricetta',
+			    bodyStyle:'padding:5px',
+			    width: 500,
+			    defaults: {width: 230},
+			    defaultType: 'textfield',
+			    items: [{fieldLabel: 'Ricetta', name: 'name', allowBlank: false}, {hideLabel: true, hidden: true, name: 'id'}]
+      });
+
+      addButtons.call(this);
+      attachReload.call(this);
+    }
 
     // Add buttons to the form.
+    function addButtons() {
 
-    form.addButton({text: 'Create'},
-		   function() {
-		     Ext.getCmp('form').submit({url:'/recipes/create.json', waitMsg:'Creating new item ...'});
-    		   },
-		   this);
+      this.form.addButton({text: 'Create'},
+			  function() {
+			    this.form.getForm().submit({url:'/recipes/create.json', waitMsg:'Creating new item ...'});
+    			  },
+			  this);
 
-    form.addButton({text: 'Update'},
-		   function() {
-		     Ext.getCmp('form').getForm().submit({url:'/recipes/update/' + this.currId + '.json', waitMsg:'Updating item ...'});
-		   },
-		   this);
+      this.form.addButton({text: 'Update'},
+			  function() {
+			    this.form.getForm().submit({url:'/recipes/update/' + this.currId + '.json', waitMsg:'Updating item ...'});
+			  },
+			  this);
 
-    form.addButton({text: 'Delete'},
-		   function() {
-		     Ext.getCmp('form').getForm().submit({url:'/recipes/delete/' + this.currId + '.json', waitMsg:'Deleting item ...'});
-		   },
-		   this);
+      this.form.addButton({text: 'Delete'},
+			  function() {
+			    this.form.getForm().submit({url:'/recipes/delete/' + this.currId + '.json', waitMsg:'Deleting item ...'});
+			  },
+			  this);
+    }
 
-    // Attach dsReload to actioncomplete event.
+    // Reload datastore when an actioncomplete event is triggered.
+    function attachReload() {
 
-    form.on('actioncomplete', dsReload.createDelegate(this.ds));
+      // Code block used to reloading datastore.
+      // The block is called in the scope of this.
+      var dsReload = function() {
+	this.reload();
+      };
 
-    form.on('actionfailed', function(){
-		//handle actionfailed event
-    });
+      // Attach dsReload to actioncomplete event.
 
-    // The currently selected id.
+      this.form.on('actioncomplete', dsReload.createDelegate(this.ds));
 
-    var currId;
-
-    // Create a RowSelectionModel object.
-
-    var rsModel = new Ext.grid.RowSelectionModel({singleSelect: true});
-
-    // Load record data in the form's fields when a 'rowselect' event occurs.
-
-    rsModel.addListener('rowselect',
-			function(sm, row, rec) {
-			  Ext.getCmp("form").getForm().loadRecord(rec);
-	    		  this.currId = rec.data.id;
-			},
-			this);
-
-    // Add the grid component and configure it. Assign rsModel to sm configuration key.
-
-    this.add({
-	       xtype: 'grid',
-	       id: 'grid',
-	       ds: this.ds,
-	       title: 'Tutte le ricette',
-	       width: 500,
-	       height: 200,
-	       columns: [{header: 'Nome', width: 120, sortable: true, dataIndex: 'name'}],
-	       sm: rsModel
-    });
+      // Handle actionfailed event.
+      this.form.on('actionfailed', function() {  });
+    }
 
   },
 
   // Definition of a public method for the component.
+  // But how to get access to component properties?
 
   aMethod: function() {
-    return "A public method.";
+    alert('Public method.');
   }
 
 });
