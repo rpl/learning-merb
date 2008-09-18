b63a8b914a620a1f0407cbe5ee1376818dea480b
Luca Greco <luca.greco@alcacoop.it>
2008-09-18 03:15:21 +0200

    refactor gridformbinding to extend panel

--- examples/restful_chef/public/js/ext.ux.gridformbinding.js
+++ examples/restful_chef/public/js/ext.ux.gridformbinding.js
@@ -1,93 +1,74 @@
-Ext.ux.GridFormBinding = Ext.extend(Ext.grid.GridPanel, {
+Ext.ux.GridFormBinding = Ext.extend(Ext.Panel, {
 
   initComponent: function() {
 
-    // Apply configuration.
+    // Apply configuration
+    var grid_def = Ext.apply(this,
+      {
+	xtype: 'grid',
+	sm: new Ext.grid.RowSelectionModel({
+	  listeners: {
+	    rowselect: function(sm, row, rec) {
+    	      internal_form.getForm().loadRecord(rec);
+    	      this.currId = rec.data.id;
+	    }
+	  }
+	})
+      });
+
+    var form_def = Ext.apply(this.form,
+      {
+	  xtype: 'form',
+	  id: 'form',
+	  bodyStyle: {
+	    border: 0,
+	    padding: '10px 0 10px 0'
+	  },
+	  collapsible: true,
+	  titleCollapse: true,
+	  listeners: {
+	    actioncomplete: function() {
+	      internal_grid.store.reload();
+	    },
+	    actionfailed: function() {
+	      alert("FAILED");
+	    }
+	  },
+	  buttons: [
+	    {
+	      text: 'Create',
+	      handler:  function() {
+	       internal_form.getForm().submit({url:'/recipes/create.json', waitMsg:'Creating new item ...'});
+    	      }
+	    },
+	    {
+	      text: 'Update',
+	      handler:  function() {
+		internal_form.getForm().submit({url:'/recipes/update/' + this.currId + '.json', waitMsg:'Updating item ...'});
+    	      }
+	    },
+	    {
+	      text: 'Delete',
+	      handler:  function() {
+		internal_form.getForm().submit({url:'/recipes/delete/' + this.currId + '.json', waitMsg:'Deleting item ...'});
+    	      }
+	    }
+	  ]
+	}
+    );
+
+    var internal_grid = new Ext.grid.GridPanel(grid_def);
+    var internal_form = new Ext.form.FormPanel(form_def);
+
     Ext.apply(this, {
-		tbar: [{text: 'New'}, {text: 'Delete'}],
-		id: 'grid'
-	      });
+      tbar: [{text: 'New'}, {text: 'Delete'}],
+      id: 'gridpanel',
+      items: [internal_form,internal_grid]
+    });
 
     // Call superclass constructor.
-    Ext.ux.GridFormBinding.superclass.initComponent.call(this, arguments);
-
-    initForm.call(this);
-    addSelectionEvents.call(this);
-
-    // private methods
-
-    // Load selected record in the form field when a rowselect
-    // event is triggered.
-    function addSelectionEvents() {
-      this.selModel.addListener('rowselect',
-    				function(sm, row, rec) {
-    				  Ext.getCmp("form").getForm().loadRecord(rec);
-    	    			  this.currId = rec.data.id;
-    				},
-    				this);
-
-    }
-
-    // Add the form component into the panel and configure it.
-    function initForm() {
-
-      this.form = this.add(Ext.apply({
-				       xtype: 'form',
-				       id: 'form',
-				       bodyStyle: {
-					 border: 0,
-					 padding: '10px 0 10px 0'
-				       },
-				       collapsible: true,
-				       titleCollapse: true
-				     },
-				     this.form));
-
-      addButtons.call(this);
-      attachReload.call(this);
-    }
-
-    // Add buttons to the form.
-    function addButtons() {
-
-      this.form.addButton({text: 'Create'},
-			  function() {
-			    this.form.getForm().submit({url:'/recipes/create.json', waitMsg:'Creating new item ...'});
-    			  },
-			  this);
-
-      this.form.addButton({text: 'Update'},
-			  function() {
-			    this.form.getForm().submit({url:'/recipes/update/' + this.currId + '.json', waitMsg:'Updating item ...'});
-			  },
-			  this);
-
-      this.form.addButton({text: 'Delete'},
-			  function() {
-			    this.form.getForm().submit({url:'/recipes/delete/' + this.currId + '.json', waitMsg:'Deleting item ...'});
-			  },
-			  this);
-    }
-
-    // Reload datastore when an actioncomplete event is triggered.
-    function attachReload() {
-
-      // Code block used to reloading datastore.
-      // The block is called in the scope of this.store
-      var dsReload = function() {
-	this.reload();
-      };
-
-      // Attach dsReload to actioncomplete event.
-
-      this.form.on('actioncomplete', dsReload.createDelegate(this.store));
-
-      // Handle actionfailed event.
-      this.form.on('actionfailed', function() {  });
-    }
-
+    Ext.ux.GridFormBinding.superclass.initComponent.call(this);
   }
-
 });
 
 Ext.reg('gridformbinding', Ext.ux.GridFormBinding);
